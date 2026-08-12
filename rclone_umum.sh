ce80d6ab0f7f3abef383537704301f288b96c397
Ricky D Surya <101728184+rickyds-bot@users.noreply.github.com>
2026-08-12 12:14:13 +0700

    Add files via upload

---
+++ rclone_umum.sh
@@ -0,0 +1,56 @@
+
+#!/bin/bash
+
+# === CONFIGURABLE ===
+SOURCE_FOLDER="/data2/umum"
+MEGA_REMOTE="mega"
+MEGA_FOLDER="backup_umum"
+LOG_DIR="/root/log"
+DATE=$(date '+%d-%m-%Y')
+TIME=$(date '+%Y-%m-%d %H:%M:%S')
+LOG_FILE="$LOG_DIR/$DATE-backup_um.log"
+
+# === PREPARE LOG FOLDER ===
+#mkdir -p "$LOG_DIR"
+
+# === START BACKUP ===
+echo "[$TIME] Starting incremental backup from $SOURCE_FOLDER to $MEGA_REMOTE:/$MEGA_FOLDER" >> "$LOG_FILE"
+
+rclone copy "$SOURCE_FOLDER" "$MEGA_REMOTE:/$MEGA_FOLDER" \
+   --progress \
+   --log-file="$LOG_FILE" \
+   --log-level INFO \
+   --update \
+   --create-empty-src-dirs \
+   --exclude '*.tmp' \
+   --exclude '*.lock' \
+   --ignore-errors
+
+if [ $? -eq 0 ]; then
+   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backup completed successfully" >> "$LOG_FILE"
+else
+   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backup failed!" >> "$LOG_FILE"
+fi
+                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
